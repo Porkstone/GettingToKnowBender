52a363311490f385b719325800357715cda0fe0f
Charlie Barker <Charlie.Barker@wonga.com>
2012-12-17 17:53:15 +0000

    Added XmlElement attribute to Security check command to remove xmlns when serialising to xml

--- SecurityCheckCommand.cs
+++ SecurityCheckCommand.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Xml.Serialization;
 
 namespace GettingToKnowBender
 {
     public partial class SecurityCheckCommand
     {
+        [XmlElement(Type = typeof(Guid))]
         public Object AccountId { get; set; }
+        [XmlElement(Type = typeof(Guid))]
         public Object RequestId { get; set; }
         public SecurityQuestionAnswer FirstSecurityQuestion { get; set; }
         public SecurityQuestionAnswer SecondSecurityQuestion { get; set; }
